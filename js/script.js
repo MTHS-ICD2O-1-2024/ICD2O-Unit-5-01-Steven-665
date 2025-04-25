30caf072cfde344cb8955c711f12e00e983b6cdc
Yi-Steven-Zhou <yi.zhou@stu.ocsb.ca>
2025-04-25 16:38:46 +0000

    add icon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: yi.zhou
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random())
+
+/**
+* This function checks if the user has selected the random number from above
+*/
+// eslint-disable-next-line no-unused-vars
+function Check() {
+  // input
+  const userNumber = parseInt(document.getElementById('user-number').value)
+
+  // process
+  if (userNumber === randomNumber) {
+    // output
+    document.getElementById('result').innerHTML =
+      '<p>You have guessed the correct number!</p>'
+
+  }
+
+  if (userNumber !== randomNumber) {
+    // output
+    document.getElementById('result').innerHTML =
+      '<p>You have guessed the wrong number!' +
+      '<br/>The correct number was: ' + randomNumber + '</p>'
+  }
+}
